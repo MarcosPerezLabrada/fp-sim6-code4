42bc99092e473154a020325d3c9a59230fe04d1a
Marcos Perez <marcosperezlabrada2@gmail.com>
2023-01-19 19:41:16 -0500

    Changing testing method from jest to cypress

---
+++ cypress/e2e/controllers/E2E.cy.js
@@ -0,0 +1,14 @@
+const passport = require("passport")
+
+describe('testing redirections when not logged', function () {
+
+  it("testing security 1",()=> {
+    cy.visit("/trails")
+    cy.url().should("contain","google")
+  })
+  it("testing security 1",()=> {
+    cy.visit("trails/new")
+    cy.url().should("contain","google")
+  })
+  
+})
