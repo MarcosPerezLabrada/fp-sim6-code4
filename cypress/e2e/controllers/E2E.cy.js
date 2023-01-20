a973a1b3e8d33cb4e5dba6d179b537f953b03743
Marcos Perez <marcosperezlabrada2@gmail.com>
2023-01-19 22:42:43 -0500

    same bug than before

--- cypress/e2e/controllers/E2E.cy.js
+++ cypress/e2e/controllers/E2E.cy.js
@@ -1,12 +1,10 @@
-const passport = require("passport")
-
 describe('testing redirections when not logged', function () {
 
   it("testing security 1",()=> {
     cy.visit("/trails")
     cy.url().should("contain","google")
   })
-  it("testing security 1",()=> {
+  it("testing security 2",()=> {
     cy.visit("trails/new")
     cy.url().should("contain","google")
   })
